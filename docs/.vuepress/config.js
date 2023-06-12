93b83734cacee303467fc7847a3aa3df1bfee72d
jaemyunlee <jjoljjollee@gmail.com>
2023-06-12 17:54:19 +0900

    edit vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ export default defineUserConfig({
   lang: 'ko-KR',
   title: 'k8s network',
   description: 'learn kubernetes network with Minikube',
-
+  base: '2023-k8s-community-day-seoul',
   theme: defaultTheme({
     logo: '/images/logo.svg',
   }),
